ff8f73cb2dded0943162e629a42fbdf75785b3c4
Mizchelle Rose Sabangan <sabanganmizcy@gmail.com>
2023-10-14 17:12:20 -1200

    Final changes

--- BSIT3L-Movies/Controllers/MoviesController.cs
+++ BSIT3L-Movies/Controllers/MoviesController.cs
@@ -12,6 +12,7 @@ public class MoviesController : Controller
             Image = "~/css/images/movie.1.jpeg",
             Description = "Bird Box Barcelona is a 2023 Spanish post-apocalyptic horror thriller film directed and written by Álex and David Pastor. A spin-off sequel to the 2018 film Bird Box, adapted from Josh Malerman's 2014 novel of the same name, it was released on Netflix on July 14, 2023 to mixed reviews.",
             WikipediaLink = "https://en.wikipedia.org/wiki/Bird_Box_Barcelona"
+
         };
 
         return View(movie);
